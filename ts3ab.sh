41d14519b7d78d48b01a6ff6326392096612e501
Adrian | LogiCookie <68113413+logicookie@users.noreply.github.com>
2021-03-15 09:42:23 +0100

    Add files via upload

---
+++ ts3ab.sh
@@ -0,0 +1,40 @@
+clear
+echo "DeinMusikbot.de"
+echo "made by init11.de"
+sleep 5
+clear
+echo "start..."
+apt update -y
+clear
+apt upgrade -y
+clear
+apt install libopus-dev unzip ffmpeg sudo screen htop -y
+clear
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb -y
+cd /home
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-3.1 
+sudo apt-get install -y dotnet-runtime-3.1
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-3.1
+clear
+mkdir TS3AudioBot
+cd TS3AudioBot
+wget --no-check-certificate  https://data.init11.de/TS3AudioBot.zip
+unzip TS3AudioBot.zip 
+rm TS3AudioBot.zip
+sleep 1
+clear
+echo "finished"
+echo "manuel finish required"
+
